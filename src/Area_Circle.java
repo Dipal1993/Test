163527093524d8c5346b93c9dc208ec6ad35b966
your name <soft.testing1993@gmail.com>
2024-03-10 20:27:41 +0000

    Revert "Area of circle"
    This reverts commit 7624415a1b1a8675c6e9d205db29fa58a0da3a11.

--- src/Area_Circle.java
+++
@@ -1,9 +0,0 @@
-
-
-    public class Area_Circle {
-        static final double pia =3.14;
-        public static void main(String[] args) {
-            double r=20.23;
-            System.out.println("Area of circle : "+(2*pia*r));
-        }
-    }
