c23af4bfaef5ec03d2c8a382a6179fcfd62a7248
your name <soft.testing1993@gmail.com>
2024-03-10 18:59:39 +0000

    Revert "Change in first file"
    This reverts commit 9bf51f8b7a4d513725ff122d6e570ed3808ee834.

--- src/Firstjavaclass.java
+++ src/Firstjavaclass.java
@@ -2,7 +2,6 @@ public class Firstjavaclass {
     public static void main(String[] args) {
         System.out.println("JAVA class");
         System.out.println("My class");
-        System.out.println("Data file is Commited");
     }
 
 }
